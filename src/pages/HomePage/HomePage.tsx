e01127c25f19cab745b11f201f0b3831ec166373
Ingvild Sandven <ingviksa@stud.ntnu.no>
2026-02-09 22:48:34 +0100

    [backend] Init of backend with supabase (#10)

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,5 +1,18 @@
-function HomePage(){
-    return(
-        <div>Henriette Sandven</div>
-    );
-} export default HomePage;
+import { useEffect } from "react";
+import supabase from "../../api/supabase/client";
+
+function HomePage() {
+  useEffect(() => {
+    (async () => {
+      try {
+        let { data: posts, error } = await supabase.from("posts").select("*");
+        console.log(posts, error);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    })();
+  }, []);
+
+  return <div>Henriette Sandven</div>;
+}
+export default HomePage;
