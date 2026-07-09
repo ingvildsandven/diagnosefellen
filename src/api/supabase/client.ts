09fb6937d330d869be108e9632f653796c04c8ba
Ingvild Sandven <ingviksa@stud.ntnu.no>
2026-07-09 10:58:06 +0200

    updated favicon link

--- src/api/supabase/client.ts
+++ src/api/supabase/client.ts
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = 'https://fjbsmoxcydsifodupmoo.supabase.co';
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseKey = import.meta.env.VITE_SUPABASE_PUBLISH_KEY;
 const supabase = createClient(supabaseUrl, supabaseKey);
 
